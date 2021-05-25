4039a7c6352badea73580c6444d023f44286a7e7
Francescapievani <71377222+Francescapievani@users.noreply.github.com>
2021-05-25 12:06:02 +0200

    buttons

--- sketch.js
+++ sketch.js
@@ -20,4 +20,13 @@ imageMode(CENTER);
 
 image(myLogo,myImage.width/2, myImage.height/2-50);
 image (myImage, myImage.width/2 +150, myImage.height/2-50);
+
+
+//buttons
+buttonHome = createButton("Home");
+buttonHome.position(windowWidth/4, windowHeight/13);
+buttonUtente = createButton("Utente");
+buttonUtente.position(windowWidth/3, windowHeight/13);
+buttonEnte = createButton("Ente");
+buttonEnte.position(windowWidth/3+150, windowHeight/13) ;
 }
