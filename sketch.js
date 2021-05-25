67ce2b458b7fde55aebf80257665d0c1719a1087
Francescapievani <71377222+Francescapievani@users.noreply.github.com>
2021-05-25 11:46:58 +0200

    update 25/05

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let myText1;
 
 
 function preload(){
-myImage = loadImage("./assets/scimmia01.png");
+myImage = loadImage("./assets/scimmia1.png");
 myLogo = loadImage("./assets/logo.png");
 }
 
@@ -17,5 +17,7 @@ function setup() {
 
 function draw() {
 imageMode(CENTER);
+
 image(myLogo,myImage.width/2, myImage.height/2-50);
 image (myImage, myImage.width/2 +150, myImage.height/2-50);
+}
